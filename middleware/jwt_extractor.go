9b73c6e0afcedde06f11c7a405aafaa79c1e944c
Tavee Khunbida <taveek@gmail.com>
2019-08-27 09:35:46 +0700

    init

---
+++ middleware/jwt_extractor.go
@@ -0,0 +1,44 @@
+package middleware 
+
+import (
+  "strings"
+  "net/http"
+  "github.com/dgrijalva/jwt-go"
+  "github.com/labstack/echo/v4"
+)
+
+type JWTExtractorConfig struct {
+  DataFields []string
+}
+
+func JWTExtractor(config JWTExtractorConfig) echo.MiddlewareFunc { 
+  return func(next echo.HandlerFunc) echo.HandlerFunc {
+    return func(c echo.Context) error {
+      val := c.Request().Header.Get("Authorization")
+      if val == "" {
+        return echo.NewHTTPError(http.StatusBadRequest)
+      }
+
+      splitToken := strings.Split(val, "Bearer ")
+      if len(splitToken) != 2 {
+        return echo.NewHTTPError(http.StatusBadRequest)
+      }
+
+      // Extract claims info
+      token, _, err := new(jwt.Parser).ParseUnverified(splitToken[1], jwt.MapClaims{})
+      if err != nil {
+        return echo.NewHTTPError(http.StatusUnauthorized)
+      }
+      if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		for _, key := range config.DataFields {
+          if name, found := claims[key]; found {
+            c.Request().Header.Add("X-Consumer-Token-" + strings.Title(key), name.(string))
+          }
+	    }
+      }
+      // Proceed
+      return next(c)
+    }	
+  }
+}
+
