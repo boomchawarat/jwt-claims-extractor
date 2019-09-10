34a4b18ecf54e8db90b42bcc92f90dfa413fd56b
Chawarat Chaemchuen <boom.chawarat@odds.team>
2019-09-10 22:07:54 +0700

    edit go module

--- go.mod
+++ go.mod
@@ -1,14 +1,12 @@
 module github.com/boomchawarat/jwt-claims-extractor
+
 go 1.12
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-<<<<<<< HEAD
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/echo/v4 v4.1.10
-=======
+
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
->>>>>>> 870330cecebb66759d5ac032d8c1b4144554d9c8
+
 )
--- middleware/jwt_extractor.go
+++ middleware/jwt_extractor.go
@@ -33,7 +33,6 @@ func JWTExtractor(config JWTExtractorConfig) echo.MiddlewareFunc {
 			if claims, ok := token.Claims.(jwt.MapClaims); ok {
 				for _, key := range config.DataFields {
 					if name, found := claims[key]; found {
-
 						if key != "role" {
 							c.Request().Header.Add("X-Consumer-Token-"+strings.Title(key), name.(string))
 						} else {
@@ -53,7 +52,6 @@ func JWTExtractor(config JWTExtractorConfig) echo.MiddlewareFunc {
 							roleString += "]"
 							c.Request().Header.Add("X-Consumer-Token-"+strings.Title(key), roleString)
 						}
-
 					}
 				}
 			}
